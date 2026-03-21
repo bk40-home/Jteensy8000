44069fc96a2f42b6883572f503baf45916b428c7
bk40-home <bishopkris40@hotmail.com>
2026-03-21 23:23:46 +0000

    ooops
    I'll fix in morning

---
+++ hexefx_audiolib_i16.h
@@ -0,0 +1,13 @@
+#ifndef _HEXEFX_AUDIO_I16_H
+#define _HEXEFX_AUDIO_I16_H
+
+#include "effect_platereverb_i16.h"
+#include "effect_springreverb_i16.h"
+#include "effect_reverbsc_i16.h"
+#include "effect_delaystereo_i16.h"
+#include "effect_phaserStereo_i16.h"
+
+#include "mixer_synth8ch_i16.h"
+#include "synth_waveform_ext_i16.h"
+
+#endif // _HEXEFX_AUDIO_H
