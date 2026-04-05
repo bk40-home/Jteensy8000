2ce5cf20bea95f64c7c608b469720d96045176a8
bk40-home <bishopkris40@hotmail.com>
2026-04-05 19:39:23 +0100

    headers and midi feedback to ui

--- AudioFilterOBXa_OBXf.cpp
+++ AudioFilterOBXa_OBXf.cpp
@@ -1,4 +1,25 @@
-#include "AudioFilterOBXa_OBXf.h"
+/* Audio Library for Teensy
+ * Copyright (c) 2025, Kris Bishop, bishopkris40@hotmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ #include "AudioFilterOBXa_OBXf.h"
 
 // Keep math constants local
 static constexpr float OBXA_PI = 3.14159265358979323846f;
